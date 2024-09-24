e1c076365f648fec3309b6a5f009695443575169
Can Ngo <44318634+nncan99@users.noreply.github.com>
2024-09-24 15:15:20 +0700

    Initial Commit

---
+++ webpack.dev.js
@@ -0,0 +1,39 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    mode: "development",
+    entry: "./src/index.js",
+    output: {
+        filename: "main.js",
+        path: path.resolve(__dirname, "dist"),
+        clean: true,
+    },
+    devtool: "eval-source-map",
+    devServer: {
+        watchFiles: ["./src/template.html",
+            "./src/styles.css",
+            "./src/index.js"]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/template.html",
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.html$/i,
+                use: "html-loader",
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif|webp|svg)$/i,
+                type: "asset/resource",
+            },
+        ],
+    },
+};
